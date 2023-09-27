3c07da6c9d8ad3d643198fbf2244e726fd89ef5d
River <riverdejesus2@gmail.com>
2023-09-27 17:50:51 -0400

    create genre model

---
+++ src/main/java/model/Genre.java
@@ -0,0 +1,71 @@
+package model;
+
+
+public class Genre {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private User user;
+
+    public Genre() {
+    }
+
+    public Genre(Long id, String name, String description, User user) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Genre(String name, String description, User user) {
+        this.name = name;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
