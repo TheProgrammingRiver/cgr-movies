9e310f85c270df9e47ab182e5ea67523afa8f43d
River <riverdejesus2@gmail.com>
2023-09-27 17:44:20 -0400

    add jpa annotation

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,19 +1,23 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String emailAddress;
 
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public User() {
