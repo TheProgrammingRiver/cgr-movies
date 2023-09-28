48338e8530bef3c424de276f20bdea297d9124dc
River <riverdejesus2@gmail.com>
2023-09-28 17:50:44 -0400

    Update UserService, add LoginUser

--- src/main/java/com/example/cgrmovies/service/UserService.java
+++ src/main/java/com/example/cgrmovies/service/UserService.java
@@ -2,7 +2,16 @@ package com.example.cgrmovies.service;
 
 import com.example.cgrmovies.exception.InformationNotFoundException;
 import com.example.cgrmovies.model.User;
+import com.example.cgrmovies.model.request.LoginRequest;
+import com.example.cgrmovies.security.JWTUtils;
+import com.example.cgrmovies.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.cgrmovies.repository.UserRepository;
 
@@ -13,13 +22,23 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTUtils jwtUtils;
+
+    private final AuthenticationManager authenticationManager;
+
     /**
      * Injects dependencies and enables userService to access the resources.
      * @param userRepository
      */
     @Autowired
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository,@Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
     }
 
     /**
@@ -33,6 +52,19 @@ public class UserService {
             return userOptional.get();
         } else {
             throw new InformationNotFoundException("EmailAddress not found");
+        }
+    }
+
+    public Optional<String> loginUser(LoginRequest loginRequest){
+        UsernamePasswordAuthenticationToken authenticationToken = new
+                UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e){
+            return Optional.empty();
         }
     }
 }
