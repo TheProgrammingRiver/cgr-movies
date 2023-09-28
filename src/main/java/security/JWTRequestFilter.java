626f549bf2ef9137ca4b68ba89823e04bfccc2cd
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-09-28 14:33:37 -0500

    Add Javadoc relevant to JWTRequestFilter

--- src/main/java/security/JWTRequestFilter.java
+++ src/main/java/security/JWTRequestFilter.java
@@ -20,11 +20,22 @@ public class JWTRequestFilter extends OncePerRequestFilter {
     private final JWTUtils jwtUtils;
     private final MyUserDetailsService myUserDetailsService;
 
+    /**
+     * Injects dependencies to access resources
+     * @param jwtUtils
+     * @param myUserDetailsService
+     */
     @Autowired
     public JWTRequestFilter(JWTUtils jwtUtils, MyUserDetailsService myUserDetailsService) {
         this.jwtUtils = jwtUtils;
         this.myUserDetailsService = myUserDetailsService;
     }
+
+    /**
+     * Extracts the JWT token from the request header.
+     * @param request http request
+     * @return The Token.
+     */
     public String parseJwt(HttpServletRequest request){
         final String header = request.getHeader("Authorization");
         if (StringUtils.hasText(header) && header.startsWith("Bearer")){
@@ -33,18 +44,27 @@ public class JWTRequestFilter extends OncePerRequestFilter {
         return null;
     }
 
+    /**
+     * Checks and validates JWT, loads user details,
+     * and sets the authentication token in the security context,
+     * allowing protected resources to be accessed by authenticated users.
+     *
+     * @param request
+     * @param response
+     * @param filterChain
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
-            String jwt = parseJwt(request);
+            String jwt = parseJwt(request); // get the token from request
             if (jwt != null && jwtUtils.validateJwtToken(jwt)){
                 String username = jwtUtils.getUsernameFromJwtToken(jwt);
-                UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
+                UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username); // session information
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,
-                        null, userDetails.getAuthorities());
+                        null, userDetails.getAuthorities()); //authenticates the user
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication); // grants access
             }
         } catch (Exception e){
             logger.error("unable to authenticate user.");
