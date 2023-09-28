a49210da617a9384a065a2b951aecd527db4e26c
River <riverdejesus2@gmail.com>
2023-09-28 17:29:33 -0400

    Create LoginResponse model

---
+++ src/main/java/com/example/cgrmovies/model/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.cgrmovies.model.response;
+
+public class LoginResponse {
+
+    public String jwt;
+
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
