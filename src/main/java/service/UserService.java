4a10895b7e853484bd7fbabe83c07c38d8f2204c
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-28 12:55:08 -0400

    add javadocs to UserService

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -12,10 +12,21 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    /**
+     * Injects dependencies and enables userService to access the resources.
+     * @param userRepository
+     */
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
+    /**
+     * Finds the user by the given email address
+     * @param emailAddress
+     * @return user
+     */
     public User findByEmailAddress(String emailAddress){
         Optional<User> userOptional = userRepository.findUserByEmailAddress(emailAddress);
         if (userOptional.isPresent()){
