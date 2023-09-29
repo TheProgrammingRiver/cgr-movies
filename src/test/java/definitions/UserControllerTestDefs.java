429442b2fcc078fde0c462a54d2b72c85a1b183e
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-09-29 13:56:49 -0500

    Implement aRegisteredUserLogsIn()

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -1,11 +1,17 @@
 package definitions;
 
 import com.example.cgrmovies.CgrMoviesApplication;
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
 
 import java.util.logging.Logger;
 
@@ -16,11 +22,20 @@ public class UserControllerTestDefs {
     private static final Logger logger = Logger.getLogger(UserControllerTestDefs.class.getName());
 
     @LocalServerPort
-    String port;
+    private static String port;
     private static Response response;
 
-    @Given("A user")
-    public void aUser() {
-
+    @When("A registered user logs in")
+    public String aRegisteredUserLogsIn() throws JSONException {
+        logger.info("Calling: A registered user logs in.");
+        RestAssured.baseURI = BASE_URL;
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("emailAddress", "example@email.com");
+        requestBody.put("password", "password");
+        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/auth/users/login/");
+        return response.jsonPath().getString("jwt");
     }
+
 }
