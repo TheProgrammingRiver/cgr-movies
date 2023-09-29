beb4d6de5961de97fab70c2047c1b1976f155657
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-09-29 12:04:52 -0500

    Add initial test setup to UserControllerTestDefs

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -1,8 +1,24 @@
 package definitions;
 
+import com.example.cgrmovies.CgrMoviesApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.logging.Logger;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = CgrMoviesApplication.class)
 public class UserControllerTestDefs {
+    private static final String BASE_URL = "http://localhost:";
+    private static final Logger logger = Logger.getLogger(UserControllerTestDefs.class.getName());
+
+    @LocalServerPort
+    String port;
+    private static Response response;
+
     @Given("A user")
     public void aUser() {
 
