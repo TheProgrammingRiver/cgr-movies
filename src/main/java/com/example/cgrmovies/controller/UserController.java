66c3111c028dd0e9df28b6c45fd2b5f6d404cf06
River <riverdejesus2@gmail.com>
2023-09-29 10:37:46 -0400

    Create UserController

---
+++ src/main/java/com/example/cgrmovies/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.cgrmovies.controller;
+
+
+import com.example.cgrmovies.model.request.LoginRequest;
+import com.example.cgrmovies.model.response.LoginResponse;
+import com.example.cgrmovies.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/auth/users")
+public class UserController {
+
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "/login/")
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        Optional<String>jwtToken = userService.loginUser(loginRequest);
+        if (jwtToken.isPresent()){
+            return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+        }else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body
+                    (new LoginResponse("Authentication failed "));
+        }
+    }
+}
