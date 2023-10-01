fd2b33609188e6e00057acb148b52c5b67990a9a
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-01 13:29:10 -0500

    Fix authorization logic on iViewTheListOfGenres

--- src/test/java/definitions/GenreControllerTestDefs.java
+++ src/test/java/definitions/GenreControllerTestDefs.java
@@ -19,6 +19,7 @@ public class GenreControllerTestDefs extends SetupTestDefs{
     private static final Logger log = Logger.getLogger(GenreControllerTestDefs.class.getName());
 
     private static Response response;
+    private static final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJleGFtcGxlQGVtYWlsLmNvbSIsImlhdCI6MTY5NjE4MzQ2NSwiZXhwIjoxNjk2MjY5ODY1fQ.WP40iW9Cul4JRD0lbYmGc3TdKP3YzW6l8GypWXoL4Tg";
 
     @Given("A list of genres are available")
     public void aListOfGenresAreAvailable() {
@@ -27,7 +28,7 @@ public class GenreControllerTestDefs extends SetupTestDefs{
             // Create HttpHeaders instance
             HttpHeaders headers = new HttpHeaders();
             // Set the authorization header, with a token
-            headers.set("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJleGFtcGxlQGVtYWlsLmNvbSIsImlhdCI6MTY5NjE4MzQ2NSwiZXhwIjoxNjk2MjY5ODY1fQ.WP40iW9Cul4JRD0lbYmGc3TdKP3YzW6l8GypWXoL4Tg");
+            headers.set("Authorization", "Bearer " + token);
             // Create HttpEntity with headers
             HttpEntity<String> entity = new HttpEntity<>(headers);
 
@@ -45,8 +46,11 @@ public class GenreControllerTestDefs extends SetupTestDefs{
     @When("I view the list of genres")
     public void iViewTheListOfGenres() {
         try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + token);
+            HttpEntity<String> entity = new HttpEntity<>(headers);
             ResponseEntity<String> responseEntity = new RestTemplate().exchange(BASE_URL + port + "/api/genres/",
-                    HttpMethod.GET, null, String.class);
+                    HttpMethod.GET, entity, String.class);
             List<Map<String, String>> genreList = JsonPath.from(String.valueOf(responseEntity.getBody())).get("data");
             Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
             Assert.assertTrue(genreList.size() > 0);
