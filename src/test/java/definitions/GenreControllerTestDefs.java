7b8a34e8bdd3e5b9fd6999eccd4b1aa1eb5b8710
River <riverdejesus2@gmail.com>
2023-10-02 10:53:52 -0400

    Update GenreControllerTestDefs removed duplicated method

--- src/test/java/definitions/GenreControllerTestDefs.java
+++ src/test/java/definitions/GenreControllerTestDefs.java
@@ -20,7 +20,6 @@ public class GenreControllerTestDefs extends SetupTestDefs{
     private static final Logger log = Logger.getLogger(GenreControllerTestDefs.class.getName());
 
     static Response response;
-    private static final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJleGFtcGxlQGVtYWlsLmNvbSIsImlhdCI6MTY5NjE4MzQ2NSwiZXhwIjoxNjk2MjY5ODY1fQ.WP40iW9Cul4JRD0lbYmGc3TdKP3YzW6l8GypWXoL4Tg";
 
     @Given("A list of genres are available")
     public void aListOfGenresAreAvailable() {
@@ -41,28 +40,6 @@ public class GenreControllerTestDefs extends SetupTestDefs{
         } catch (HttpClientErrorException e){
             e.printStackTrace();
         }
-    }
-
-
-    @When("I view the list of genres")
-    public void iViewTheListOfGenres() {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "Bearer " + token);
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            ResponseEntity<String> responseEntity = new RestTemplate().exchange(BASE_URL + port + "/api/genres/",
-                    HttpMethod.GET, entity, String.class);
-            List<Map<String, String>> genreList = JsonPath.from(String.valueOf(responseEntity.getBody())).get("data");
-            Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
-            Assert.assertTrue(genreList.size() > 0);
-        } catch (HttpClientErrorException e){
-            e.printStackTrace();
-        }
-    }
-
-
-    @Then("I should see the genre list")
-    public void iShouldSeeTheGenreList() {
     }
 
 
