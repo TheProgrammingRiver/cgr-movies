b8d3986bdf1231b793369ae6783312abf5ca1d3a
Courtney <63630567+courtneymcodes@users.noreply.github.com>
2023-10-01 20:48:30 -0400

    add and setup MovieControllerTestDefs

---
+++ src/test/java/definitions/MovieControllerTestDefs.java
@@ -0,0 +1,13 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+import java.util.logging.Logger;
+
+public class MovieControllerTestDefs extends SetupTestDefs{
+
+    private static final Logger log = Logger.getLogger(MovieControllerTestDefs.class.getName());
+    @Given("A list of movies are available")
+    public void aListOfMoviesAreAvailable() {
+    }
+}
