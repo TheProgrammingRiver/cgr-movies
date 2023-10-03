0397106ccc64eb0137cbeefe3604d7ae96b2d164
River <riverdejesus2@gmail.com>
2023-10-03 11:16:12 -0400

    add registerUser in UserController

--- src/main/java/com/example/cgrmovies/controller/UserController.java
+++ src/main/java/com/example/cgrmovies/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.cgrmovies.controller;
 
+import com.example.cgrmovies.model.Genre;
+import com.example.cgrmovies.model.User;
 import com.example.cgrmovies.model.request.LoginRequest;
 import com.example.cgrmovies.model.response.LoginResponse;
 import com.example.cgrmovies.service.UserService;
@@ -8,11 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/auth/users")
 public class UserController {
+
+    private HashMap<String, Object> message = new HashMap<>();
 
     private UserService userService;
     @Autowired
@@ -36,7 +41,18 @@ public class UserController {
         }
     }
 
-
+    @PostMapping(path = "/register/")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        User newUser = userService.register(user);
+        if (newUser != null) {
+            message.put("message", "User created successfully");
+            message.put("data", newUser);
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+        } else {
+            message.put("message", "Cannot create user");
+            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+        }
+    }
 }
 
 
--- src/test/resources/features/UserControllerTestFeature.feature
+++ src/test/resources/features/UserControllerTestFeature.feature
@@ -3,7 +3,7 @@ Feature: Rest API functionalities
   Scenario: Manage User access via API
     When A registered user logs in
     Then A user is authenticated
-#    When I add a new user
-#    Then A user is registered
+    When I add a new user
+    Then A user is registered
 
 
